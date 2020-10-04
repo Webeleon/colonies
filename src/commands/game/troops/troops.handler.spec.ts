e684babee1956724e511b5d03ee6c9db2bab65fd
julien prugne <julien@webeleon.dev>
2020-10-04 17:23:19 +0200

    add troops command #2

--- src/commands/game/troops/troops.handler.spec.ts
+++ src/commands/game/troops/troops.handler.spec.ts
@@ -1,11 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TroopsHandler } from './troops.handler';
+import { TroopsModule } from '../../../troops/troops.module';
+import { rootMongooseTestModule } from '../../../test-utils/mongo/MongooseTestModule';
 
 describe('TroopsHandler', () => {
   let troopsHandler: TroopsHandler;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        rootMongooseTestModule(),
+        TroopsModule,
+      ],
       providers: [TroopsHandler],
     }).compile();
 
