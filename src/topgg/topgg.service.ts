c9079a17a46e7e07d28657a8338ee8b0788680e8
Julien Prugne <julien@webeleon.dev>
2020-12-18 15:34:45 +0100

    mark player interaction on vote close #136

--- src/topgg/topgg.service.ts
+++ src/topgg/topgg.service.ts
@@ -4,6 +4,8 @@ import * as TopGG from 'dblapi.js';
 import { ConfigService } from '../config/config.service';
 import { TopggVoteRewardService } from './topgg-vote-reward/topgg-vote-reward.service';
 import { VoteReminderService } from './vote-reminder-service/vote-reminder.service';
+import { MemberService } from '../member/member.service';
+import { VoteEventArgs } from 'dblapi.js';
 
 @Injectable()
 export class TopggService {
@@ -14,9 +16,10 @@ export class TopggService {
     private readonly config: ConfigService,
     private readonly voteReward: TopggVoteRewardService,
     private readonly voteReminderService: VoteReminderService,
+    private readonly memberService: MemberService,
   ) {}
 
-  register(botId: string) {
+  register(botId: string): void {
     this.topGGClient = new TopGG(this.config.topGGToken, {
       webhookPort: this.config.topGGHookPort,
       webhookAuth: this.config.topGGHookPassword,
@@ -30,14 +33,19 @@ export class TopggService {
         'top.gg',
       );
     });
-    this.topGGClient.webhook.on('vote', async (vote) => {
+    this.topGGClient.webhook.on('vote', this.onVote(botId));
+  }
+
+  onVote(botId: string): (vote: VoteEventArgs) => Promise<void> {
+    return async (vote: VoteEventArgs) => {
       Logger.log(`User with Id ${vote.user} as voted!`, 'top.gg');
       if (vote.bot === botId) {
         Logger.log(`And for our bot!`, 'top.gg');
         await this.voteReminderService.markVote(vote.user);
+        await this.memberService.markInteraction(vote.user);
         await this.voteReward.reward(vote);
       }
-    });
+    };
   }
 
   async getWeekendMultiplier(): Promise<boolean> {
